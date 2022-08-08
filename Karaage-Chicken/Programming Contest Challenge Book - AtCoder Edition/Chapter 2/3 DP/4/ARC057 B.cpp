79cf713f77ee8bba4d958d8dc36c6804c5370623
Karaage-Chicken <keita8216.tatsu@gmail.com>
2022-08-09 00:29:00 +0900

    ARC057 B

--- Karaage-Chicken/Programming Contest Challenge Book - AtCoder Edition/Chapter 2/3 DP/4/ARC057 B.cpp
+++ Karaage-Chicken/Programming Contest Challenge Book - AtCoder Edition/Chapter 2/3 DP/4/ARC057 B.cpp
@@ -3,8 +3,8 @@ long long INF = 1 << 30;
 
 using namespace std;
 //インデックスを工夫して計算量を抑える
-//最小の勝利数でDP
-//難し
+// dp[i][j] = i日目までで機嫌がいい日がj日である最小の勝利数
+//コーナーケースが難しい
 
 int main()
 {
@@ -17,16 +17,42 @@ int main()
     for (int i = 0; i < N; i++)
     {
         cin >> a[i];
-        dp[i][0] = 0;
     }
-    dp[N][0] = 0;
-    long long s = 0;
+    dp[1][0] = 0;
+    dp[1][1] = 1;
+    long long s = a[0];
 
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i < N; i++)
     {
         for (int j = 0; j <= i; j++)
         {
-            dp[i + 1][j + 1];
+            dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i][j] + dp[i][j] * a[i] / s + 1);
+            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
         }
+        s += a[i];
     }
+
+    if (s == 0)
+    {
+        cout << 0 << endl;
+    }
+    else if (s == K)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        for (int i = N; i >= 0; i--)
+        {
+            if (dp[N][i] <= K)
+            {
+                cout << i << endl;
+                system("pause");
+                return 0;
+            }
+        }
+        cout << 0 << endl;
+    }
+    system("pause");
+    return 0;
 }
